ebea98b9faee310ae64022974eda3f71513be3ee
Wayland Chin <chinwayland@users.noreply.github.com>
2021-11-06 03:32:40 +0700

    update emails.py

--- emails.py
+++ emails.py
@@ -27,3 +27,11 @@ def send_email(message):
 	mail_server.send_message(message)
 	mail_server.quit()
 
+def generate_error_report(sender, recipient, subject, body):
+	message = email.messsage.EmailMessage()
+	message["From"] = sender
+	message["To"] = recipient
+	message["Subject"] = subject
+	message.set_content(body)
+
+	return message
